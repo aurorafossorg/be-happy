17ee2f46aa2e76df884e15121dda36f209c1e8bd
Luís Ferreira <contact@lsferreira.net>
2018-12-03 19:28:07 +0000

    work on a better splash screen
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- src/app.dart
+++ src/app.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'screen/splash.dart';
 import 'screen/home.dart';
 
 void main(){
-  runApp(new MaterialApp(
-    home: new SplashScreen(),
-    routes: <String, WidgetBuilder>{
-      '/HomeScreen': (BuildContext context) => new HomeScreen()
-    }
-  ));
+	runApp(new MaterialApp(
+		home: new HomeScreen()
+	));
 }
